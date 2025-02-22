e3e2ff9f3a853737e9928ece5485cff9d8d90042
laxmi0799 <laxmi.kumari.mec22@itbhu.ac.in>
2025-02-22 17:17:57 +0530

    new files

---
+++ site/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from "./components/About";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Router>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Features />
+              <Story />
+            </>
+          } />
+          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<div>Gallery Page</div>} />
+          <Route path="/clubs" element={<div>Clubs Page</div>} />
+          <Route path="/teams" element={<div>Teams Page</div>} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </main>
+    </Router>
+  );
+}
+
+export default App;
