8a78390ef3ce6299b747579e10f896f91b52cca4
laxmi0799 <laxmi.kumari.mec22@itbhu.ac.in>
2025-02-23 00:24:49 +0530

    story

--- site/src/components/Story.jsx
+++ site/src/components/Story.jsx
@@ -49,12 +49,13 @@ const FloatingImage = () => {
     <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
         <p className="font-general text-sm uppercase md:text-[10px]">
-          the multiversal ip world
+          celebrating art and culture
         </p>
 
         <div className="relative size-full">
           <AnimatedTitle
-            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            title="the j<b>o</b>urney of <br /> creativ<b>i</b>ty"
+
             containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
           />
 
@@ -105,15 +106,16 @@ const FloatingImage = () => {
 
         <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
           <div className="flex h-full w-fit flex-col items-center md:items-start">
-            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
-              Where realms converge, lies Zentry and the boundless pillar.
-              Discover its secrets and shape your fate amidst infinite
-              opportunities.
+          <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where passions unite, lies creativity and cultural brilliance.
+              Explore its depths and be inspired by limitless artistic
+              possibilities.
             </p>
+
 
             <Button
               id="realm-btn"
-              title="discover prologue"
+              title="discover Culture"
               containerClass="mt-5"
             />
           </div>
