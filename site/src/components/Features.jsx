4f17decace3fed25fe5ad896e6356265103681bf
laxmi0799 <laxmi.kumari.mec22@itbhu.ac.in>
2025-02-23 00:17:20 +0530

    features

--- site/src/components/Features.jsx
+++ site/src/components/Features.jsx
@@ -102,14 +102,14 @@ const Features = () => (
   <section className="bg-black pb-52">
     <div className="container mx-auto px-3 md:px-10">
       <div className="px-5 py-32">
-        <p className="font-circular-web text-lg text-blue-50">
-          Into the Metagame Layer
+      <p className="font-circular-web text-lg text-blue-50">
+          Celebrating Culture and Creativity
         </p>
         <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-          Immerse yourself in a rich and ever-expanding universe where a vibrant
-          array of products converge into an interconnected overlay experience
-          on your world.
+          Experience the vibrancy of diverse traditions and artistic expressions
+          coming together in a unified celebration of talent and culture.
         </p>
+
       </div>
 
       <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
